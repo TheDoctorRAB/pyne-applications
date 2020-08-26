74423a414b68a2b890e3c7ecfa9d1170e6149f23
TheDoctorRAB <TheDoctorRAB@localhost.localdomain>
2020-08-26 16:14:40 -0600

    update for python 3.x

--- nuclear-physics/atomic.mass.py
+++ nuclear-physics/atomic.mass.py
@@ -3,14 +3,14 @@
 # @TheDoctorRAB
 # rev.30.June.2017
 ########################################################################
-# 
-# Print molecular weight 
+#
+# Print molecular weight
 #
 ########################################################################
 #
 #
 #
-####### 
+#######
 #
 # imports
 #
@@ -22,11 +22,12 @@ from pyne.data import atomic_mass
 #
 #######
 #
-# compute atomic mass 
+# compute atomic mass
 #
-print 'Type in the isotope; i.e., h2, cs137, co60'
-isotope=raw_input()
-print isotope,atomic_mass(isotope)
+print('This script uses pyne to give the atomic mass of an isotope', '\n')
+print ('Type in the isotope; i.e., h2, cs137, co60')
+isotope=input()
+print ('The atomic mass of',isotope,'is',atomic_mass(isotope))
 #
 ########################################################################
 #
